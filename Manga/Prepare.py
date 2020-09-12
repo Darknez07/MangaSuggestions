6d1fa248b2c6e33010276eebd88c17966a3c412b
RK <rishikakkar42@gamil.com>
2020-09-12 15:33:40 +0530

    Increased data retrival Time

--- Manga/Prepare.py
+++ Manga/Prepare.py
@@ -2,6 +2,6 @@ import subprocess
 try:
     stdo = subprocess.check_output(['scrapy','crawl','Manganelo','-o','Some.csv'],
                                stderr=subprocess.STDOUT,
-                               timeout=30)
+                               timeout=120)
 except:
     print("Done Gathering data")
